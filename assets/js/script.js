1e6024a08aed799cfd0c08ec219d069e18a9bfe3
unknown <541231025@student.smktelkom-pwt.sch.id>
2024-08-28 11:29:15 +0700

    login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 document.getElementById('loginForm').addEventListener('submit', function(event){
     event.preventDefault();
 
+    // deklarasi username dan password yang benar
     const correctUsername = "Atha"
     const correctPassword = "1234"
 
+    // mengambil username dan password yang didapat
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
+    // pengkondisian ketika username dan password benar maupun salah
     if  (username === correctUsername && password === correctPassword) {
         alert("Login berhasil");
         window.location.href = 'index.html';
     }else{
         alert("Username atau password salah");
     }
-})
+});
